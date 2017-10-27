f4eb59e7888eb3a11406623c4c89a6f21a89f71e
benaryorg <binary@benary.org>
2017-10-27 11:43:50 +0200

    example source code
    Signed-off-by: benaryorg <binary@benary.org>

---
+++ src/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+int main(int argc,char **argv)
+{
+	for(int i=1;i<argc;i++)
+	{
+		printf("%s\n",argv[i]);
+	}
+
+	return 0;
+}
+
